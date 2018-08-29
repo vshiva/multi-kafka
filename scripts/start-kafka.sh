5dee32fe69fe33912821deede8584be99f167121
Scott Clasen <scott@heroku.com>
2018-08-29 15:28:08 -0700

    bump to 1.1.1

--- scripts/start-kafka.sh
+++ scripts/start-kafka.sh
@@ -2,7 +2,7 @@
 
 props_file=$KAFKA_HOME/config/$1
 
-$KAFKA_HOME/bin/config.py $props_file ${KAFKA_HOSTNAME:-"0.0.0.0"}
+$KAFKA_HOME/bin/config.py $props_file ${KAFKA_HOSTNAME:-"localhost"}
 
 # Run Kafka
 $KAFKA_HOME/bin/kafka-server-start.sh $props_file
